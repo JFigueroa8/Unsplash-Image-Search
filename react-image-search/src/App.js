598c031e1b953767d2c4ae30bb1be88360faa92f
Julmar Figueroa <julmarfigueroa@gmail.com>
2019-06-19 15:17:34 -0400

    Completed React Image Search

--- react-image-search/src/App.js
+++ react-image-search/src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import API from './API';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      title: 'React Image Search',
+      searchTerm: '',
+      loading: false,
+      images: []
+    };
+  }
+
+  formSubmitted(event) {
+    event.preventDefault();
+
+    this.setState({
+      loading: true,
+      images: []
+    })
+
+    API.search(this.state.searchTerm)
+      .then(images => {
+        this.setState({
+          loading: false,
+          images
+        })
+      })
+  }
+
+  searchTermChanged(event) {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
+
+  render() {
+    const { title, searchTerm, loading, images } = this.state; // destructuring
+    return (
+      <div>
+        <h1>{title}</h1>
+        <form onSubmit={(event) => this.formSubmitted(event)}>
+          <label htmlFor="searchTerm">Search Term</label>
+          <input onChange={(event) => this.searchTermChanged(event)} value={searchTerm} className="u-full-width" type="text" id="searchTerm" name="searchTerm" autoComplete="off" />
+          <button type="submit">Search</button>
+        </form>
+        {loading ? <img src="https://i.imgur.com/LVHmLnb.gif" alt="loading ice cream"></img> : ''}
+        <section className="images">
+          {images.map((image, i) => {
+            return <img key={image.id} alt={image.alt_description} src={image.urls.small}/>
+          })}
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
