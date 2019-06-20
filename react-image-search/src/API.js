3932d5be5675794fb3649c18cd7be580facc562d
Julmar Figueroa <julmarfigueroa@gmail.com>
2019-06-19 20:19:26 -0400

    Pull 30 pictures instead of 10

--- react-image-search/src/API.js
+++ react-image-search/src/API.js
@@ -1,4 +1,4 @@
-const API_URL = `https://api.unsplash.com/search/photos?client_id=${add_your_api_key_here}&page=1&query=`;
+const API_URL = `https://api.unsplash.com/search/photos?client_id=${add_your_api_key_here}&page=1&per_page=30&query=`;
 
 export default {
     search(searchTerm) {
